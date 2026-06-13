51be5998d696c2b3c5252a0cc0620bb24fb9986a
uvewexyz <muhammadaditiyarakhman@gmail.com>
2026-06-13 19:45:45 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,13 @@
+// ==========================================================================
+// Script JS untuk pesan notif pop-up
+// ==========================================================================
+
+const tombolSapa = document.getElementById('btnSapa');
+
+// Event Listener ketika tombol diklik
+tombolSapa.addEventListener('click', function() {
+    
+    // Menampilkan pesan notif pop-up (alert)
+    alert('Halo! Terima kasih sudah mampir ke halaman CV saya. Hidup UNSIA!');
+    
+});
